80c44087bb01ce9c16f98b7336af12566236cc15
Vincent Dallaire <vincent.dallaire@dti.ulaval.ca>
2023-08-30 16:28:09 -0400

    Initial files

---
+++ vue/main.unit.ts
@@ -0,0 +1,3 @@
+import Main from './main.vue';
+
+console.log(Main);
---
+++ vue/main.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>Main</div>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from 'vue';
+
+    export default defineComponent({
+        
+    });
+</script>
