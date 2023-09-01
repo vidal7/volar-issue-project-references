d7bbe45940ea3a4cfba5882584191f8fa94e3e3a
Vincent Dallaire <vincent.dallaire@dti.ulaval.ca>
2023-09-01 09:24:05 -0400

    Add script to create dummy files

---
+++ create-ts-files.js
@@ -0,0 +1,8 @@
+import { writeFileSync } from 'fs';
+
+['performant', 'non-performant'].forEach(name => {
+    for(var i=1; i<=3; i++) {
+        writeFileSync(name + '/' + name + '-file' + i + '.ts', 'export const a: number = 0;');
+        writeFileSync(name + '/' + name + '-file' + i + '.unit.ts', 'import { a } from "./' + name + '-file' + i + '";\n\nconsole.log(a);');
+    }
+});
